f65fee57c55294c1e0f81edbc906d835f88f7773
Kirill <131515003+Farygeric@users.noreply.github.com>
2023-11-29 16:34:55 +0300

    Update habr_pars.py

--- habr_pars.py
+++ habr_pars.py
@@ -13,11 +13,9 @@ def get_data(ur):
     soup = BeautifulSoup(req.content, "html.parser")
     href_of_the_article = soup.find_all(class_="tm-title__link")  
     for item in range(0, 4):  # start sorted
-        count = 0
-        https = f"https://habr.com{href_of_the_article[count].get('href')}"
+        https = f"https://habr.com{href_of_the_article[item].get('href')}"
         a.append(https)  # OMG a lot has been written. WHY GOD?? WHYYY
-        count += 1
-    with open("habr.json", 'w') as file:
-        json.dump(a, file, indent=0, ensure_ascii=False)
+    for i in a:
+        print(a)
 
 get_data(url)
